60217626c28c4d33e6208181f6b556edb459133a
EmBerrrrrr <lan18051973@gmail.com>
2024-02-01 21:39:12 +0700

    LoginPage

--- src/Component/LoginPage.js
+++ src/Component/LoginPage.js
@@ -1,62 +1,25 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
-// TODO remove, this demo shouldn't need to reset the theme.
-
-const defaultTheme = createTheme();
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { FaGoogle } from "react-icons/fa";
 
 export default function LoginPage() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
   };
 
   return (
-
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-
     <Box sx={{
       backgroundImage: "url('https://interiordesign.net/wp-content/uploads/2024/01/Interior-Design-Best-of-Year-2023-Steven-Harris-Architects-Rees-Roberts-Partners-idx231201_boy_BeachHouseL01b.jpg')",
       backgroundSize: 'cover',
@@ -77,7 +40,6 @@ export default function LoginPage() {
             
           }}
         >
-
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
@@ -121,20 +83,20 @@ export default function LoginPage() {
                 </Link>
               </Grid>
               <Grid item>
+                Don't have an account?
                 <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
+                  <a href="http://localhost:3002/SignUp"> SignUp</a>
                 </Link>
               </Grid>
             </Grid>
           </Box>
+          <p class="w-100 text-center">&mdash; Or Sign In With &mdash;</p>
+          <a href="https://accounts.google.com/signin/v2/usernamerecovery?hl=vi&theme=glif&flowName=GlifWebSignIn&flowEntry=ServiceLogin" class="px-2 py-2 ml-md-1 rounded">
+					<FaGoogle />
+          <span class="fa fa-google"></span>-Google
+					</a>
         </Box>
-
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    </ThemeProvider>
-
       </Container>
     </Box>
-
   );
 }
