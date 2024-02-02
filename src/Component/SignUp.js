1eb2a91b28d81ca11871a19c54a12fcda47fbcaf
dungtran2812 <dunghoclaptrinh2812@gmail.com>
2024-02-02 20:59:03 +0700

    Update SignUp.js

--- src/Component/SignUp.js
+++ src/Component/SignUp.js
@@ -5,12 +5,10 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
-
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { FaGoogle } from "react-icons/fa";
 import { Link } from 'react-router-dom';
@@ -19,7 +17,7 @@ function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
+      <Link color="inherit" to="/signup">
         Your Website
       </Link>{' '}
       {new Date().getFullYear()}
