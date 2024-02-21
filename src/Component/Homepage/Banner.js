0448f2add3d1b9b78bdcbac0809d5a407f5954dc
dungtran2812 <dunghoclaptrinh2812@gmail.com>
2024-02-21 14:02:00 +0700

    Update Banner.js

--- src/Component/Homepage/Banner.js
+++ src/Component/Homepage/Banner.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Slider from 'react-slick';
-import { Box, Button, Card, CardActions, CardContent, CardMedia, Paper, Typography } from '@mui/material';
+import { Box, Button, CardActions, CardContent, CardMedia, Paper, Typography } from '@mui/material';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
